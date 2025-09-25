d96cdc4578785d707adcfa93d407fae533206629
Farah <farahdoll5211@gmail.com>
2025-09-25 10:21:18 -0400

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="farah@google.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+
+// accountId = 2 //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword ="212121"
+accountCity = "Bengalur"
+
+console.log (accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
