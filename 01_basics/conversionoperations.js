4fc540fe97f6264b70f9eb7a7464a71c8eb2ce0f
Farah <farahdoll5211@gmail.com>
2025-09-26 07:13:39 -0400

    conversionofdatatypes

---
+++ 01_basics/conversionoperations.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console. log(typeof score);
+//console. log(typeof(score));
+
+let valueInNumber = Number(score)
+//console. log(typeof valueInNumber);
+//console. log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "farah"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console. log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console. log(stringnumber) ;
+console. log(typeof stringnumber) ;
