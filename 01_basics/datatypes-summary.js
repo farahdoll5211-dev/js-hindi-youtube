715126b70038b74322010a64b0912214b2489fe9
Farah <farahdoll5211@gmail.com>
2025-09-30 06:05:01 -0400

    datatypes-summary.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+
+const id = Symbol ('123')
+const anotherId = Symbol ('123')
+
+//console. log(id === anotherId);
+
+//const BigNumber = 34565576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["amir", "shahrukh", "saif"];
+let myobj = {
+    name: "farah",
+    age: "24",
+} 
+
+const myFunction = function(){
+    console. log("Hello world");
+}
+
+//console. log(typeof bignumber);
+//console. log(typeof outsideTemp);
+//console. log(typeof scoreValue);
+//console. log(typeof myFunction);
+console. log(typeof anotherId);
+
+
