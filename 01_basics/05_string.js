5863f56dc62f5b2a99a99124518d42abb13abe48
Farah <farahdoll5211@gmail.com>
2025-10-01 07:24:33 -0400

    string info

---
+++ 01_basics/05_string.js
@@ -0,0 +1,40 @@
+const name = "farah"
+const repoCount = "50"
+
+//console.log(name + repoCount +" Value");
+
+//console. log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('farahah-hc')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+//nsole.log(gameName.toUpperCase());
+//console. log(gameName.charAt('2'));
+//nsole.log(gameName.indexOf('r'));
+
+const newString = gameName.substring(0, 4)
+//nsole.log(newString);
+
+const anotherString = gameName.slice(-9, 4)
+console.log(anotherString);
+
+const newStringOne = "   farah    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://farah.com/farah%20doll"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('farah'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
